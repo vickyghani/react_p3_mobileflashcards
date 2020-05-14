b1ae2136163682e2f324f26edb12d2751f59276a
M Vicky Ghani Aziz <30381081+vickyaziz@users.noreply.github.com>
2020-05-15 05:45:23 +0700

    Initial commit
    Initial commit

---
+++ utils/_DATA.js
@@ -0,0 +1,28 @@
+export const decks = {
+    React: {
+      title: 'React',
+      questions: [
+        {
+          question: 'is React a JavaScript library for building user interfaces?',
+          answer: 'true'
+        },
+        {
+          question: 'is React maintaned by Microsoft and Apple together?',
+          answer: 'false'
+        }
+      ]
+    },
+    JavaScript: {
+      title: 'JavaScript',
+      questions: [
+        {
+          question: 'is JavaScript the same as Java?',
+          answer: 'false'
+        },
+        {
+          question: 'is JavaScript a programming language?',
+          answer: 'true'
+        }
+      ]
+    }
+  }
