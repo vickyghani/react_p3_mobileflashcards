ef9f744f48b686b8d40ed1825751b2287ca99d43
M Vicky Ghani Aziz <30381081+vickyaziz@users.noreply.github.com>
2020-05-15 14:45:10 +0700

    Update colors.js

--- utils/colors.js
+++ utils/colors.js
@@ -1,11 +1,8 @@
-// utils/colors.js
-
 export const purple = '#292477'
 export const white = '#fff'
 export const orange = '#f26f28'
 export const lightPurp = '#7c53c3'
 export const pink = '#b93fb3'
-
 export const gray = '#757575'
 export const green = '#9acb34'
 export const red = '#e7301c'
